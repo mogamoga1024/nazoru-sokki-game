d79879831265e8196cf447c4f93a5d22c4e44c4a
mogamoga <mogamoga.g1thu6@gmail.com>
2024-12-10 00:09:29 +0900

    捨てるなんてとんでもない

---
+++ color.js
@@ -0,0 +1,15 @@
+
+const color = {
+    line4: {
+        r: 205, g: 133, b: 63,
+        hex: "#CD853F"
+    },
+    line8: {
+        r: 85, g: 107, b: 47,
+        hex: "#556B2F"
+    },
+    line16: {
+        r: 204, g: 204, b: 102,
+        hex: "#CCCC66"
+    },
+};
