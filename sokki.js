246f5633ee9680d51dab8ed47d4164614bfa185c
mogamoga <mogamoga.g1thu6@gmail.com>
2024-12-10 00:42:30 +0900

    誰とも違う私だけの世界

---
+++ sokki.js
@@ -0,0 +1,37 @@
+
+function sokkiTest(actualList, expectedList) {
+    let isOK = false;
+    if (actualList.length === expectedList.length) {
+        for (let i = 0; i < actualList.length; i++) {
+            const aDif = actualList[i];
+            let eDif = expectedList[i];
+
+            if (eDif === "-1/2") {
+                eDif = expectedList[i - 1] * -1 / 2;
+            }
+
+            if (eDif === "+") {
+                isOK = Math.sign(aDif) > 0;
+            }
+            else if (eDif === "-") {
+                isOK = Math.sign(aDif) < 0;
+            }
+            else if (
+                Math.sign(aDif) === Math.sign(eDif) &&
+                Math.abs(aDif) >= Math.abs(eDif) * 0.5 &&
+                Math.abs(aDif) <= Math.abs(eDif) * 1.5
+            ) {
+                isOK = true;
+            }
+            else {
+                isOK = false;
+            }
+
+            if (!isOK) {
+                break;
+            }
+        }
+    }
+    return isOK;
+}
+
