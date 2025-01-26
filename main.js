0724f33ece6a78c94235b2a04e18ed008582ed01
mogamoga <mogamoga.g1thu6@gmail.com>
2025-01-27 05:09:53 +0900

    楽しいことしよう みんな集まってさあ 今日も明日からも ずっと終わらない僕たちの夢

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
+let mondaiList = [];
+
 const app = {
     data() {
         return {
-
+            scene: "game", // top, countdown, game, result
+            mondaiListIndex: 0,
+            mondai: [],
+            kaitou: [],
+            message: "選んでね🤔",
         };
     },
     created() {
-
+        // debug
+        this.mondai = ["あ", "い", "う", "え", "お"];
+    },
+    computed: {
+        sintyoku() {
+            return `${this.mondaiListIndex}/${mondaiList.length}`;
+        },
     },
     methods: {
 
