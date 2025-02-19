c1811aaa432dccd1544b573047aa38453e0703a6
mogamoga <mogamoga.g1thu6@gmail.com>
2025-02-19 13:05:00 +0900

    りふぁ

--- sound/sound.js
+++ sound/sound.js
@@ -13,38 +13,3 @@ function loadSound(path) {
         });
     });
 }
-
-// class Sound {
-//     #audio = null;
-//     constructor(audio, volume = 1) {
-//         this.#audio = audio;
-//         this.#audio.volume = volume;
-//     }
-
-//     static loadSound(path, volume = 1) {
-//         const audio = new Audio(path);
-//         audio.load();
-//         return new Promise(resolve => {
-//             audio.oncanplaythrough = () => {
-//                 resolve(new Sound(audio, volume));
-//             };
-//             audio.onerror = () => {
-//                 resolve(new Sound(audio, volume));
-//             };
-//         });
-//     }
-
-//     play() {
-//         this.reset();
-//         this.#audio.play();
-//     }
-//     stop() {
-//         this.#audio.pause();
-//     }
-//     reset() {
-//         if (!this.#audio.paused) {
-//             this.#audio.pause();
-//         }
-//         this.#audio.currentTime = 0;
-//     }
-// }
