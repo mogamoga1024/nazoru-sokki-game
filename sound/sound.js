661502fffa3d795f051512bfdddd96d83c080f25
mogamoga <mogamoga.g1thu6@gmail.com>
2025-02-25 17:47:58 +0900

    howler イベントのフックの仕方を変えてみた

--- sound/sound.js
+++ sound/sound.js
@@ -1,39 +1,23 @@
 
-function loadSound(path, _option = null, needTimeout = true) {
-    const option = {
-        src: [path],
-        volume: 1,
-        html5: false, // trueだとユーザー操作がどうのこうので音が鳴らない場合がある。例えユーザー操作があっても。
-    };
-    if (_option !== null) {
-        Object.assign(option, _option);
-    }
-    const sound = new Howl(option);
+function loadSound(path, _option = null) {
     return new Promise(resolve => {
-        let isCompleted = false;
-        let timer = 0;
-        if (needTimeout) {
-            timer = setTimeout(() => {
-                if (isCompleted) return;
-                isCompleted = true;
-                sound.isOK = sound.state() === "loaded";
+        const option = {
+            src: [path],
+            volume: 1,
+            html5: false, // trueだとユーザー操作がどうのこうので音が鳴らない場合がある。例えユーザー操作があっても。
+            onload() {
+                sound.isOK = true;
                 resolve(sound);
-            }, 1000 * 10);
+            },
+            onloaderror() {
+                sound.isOK = false;
+                resolve(sound);
+            },
+        };
+        if (_option !== null) {
+            Object.assign(option, _option);
         }
-        sound.once("load", () => {
-            if (isCompleted) return;
-            isCompleted = true;
-            clearTimeout(timer);
-            sound.isOK = true;
-            resolve(sound);
-        });
-        sound.once("loaderror", () => {
-            if (isCompleted) return;
-            isCompleted = true;
-            clearTimeout(timer);
-            sound.isOK = false;
-            resolve(sound);
-        });
+        const sound = new Howl(option);
     });
 }
 
